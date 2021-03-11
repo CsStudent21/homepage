64d72e134995707bef118882f64630ff862208c9
CsStudent21 <tpadula@hey.com>
2021-03-11 11:58:55 +0100

    images update

---
+++ core.js
@@ -0,0 +1,17 @@
+function loadBAbooks(){
+    let BAshelf = document.querySelector("#businessAnalyticsList");ù
+    for(i = 0; i < BAbooks.length, i++) {
+        BAshelf.innerHTML += `
+        `
+    };
+
+
+}
+
+
+
+let BAbooks = [
+    {
+        
+    },
+]
